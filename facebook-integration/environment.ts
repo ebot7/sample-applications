2b5fdacf9ca6d8b281e6ed0afe937015c1c46de3
Kyrill <kyrill.langhans@e-bot7.com>
2021-06-24 12:02:16 +0200

    [AP-357] updated lambda (includes TODOs with values to replace)

---
+++ facebook-integration/environment.ts
@@ -0,0 +1,3 @@
+export default {
+    appKey: 'xapp-60d1b2cbddf51f23bffd930d-265e1e5b8c194338552a58125ece1e2a40b4d623'
+}
--- facebook-integration/handler.ts
+++ facebook-integration/handler.ts
@@ -1,23 +1,52 @@
-import { APIGatewayEvent, Callback, Context, Handler } from 'aws-lambda';
+import { APIGatewayEvent, Context, Handler } from "aws-lambda";
+import { Ebot7ConvWrapper } from "./helpers/ebot7-conv-wrapper";
+import { sendMessage } from "./helpers/facebook-send";
+import environment from "./environment";
+import { validateEvent } from "./helpers/validate-event";
+import { promises as fsPromises } from "fs";
 
-const EVENT_MESSAGE_CREATED_NAME = 'message:created';
-type Source = 'visitor' | 'bot' | 'agent';
-
-export const eventsEndpoint: Handler = (event: APIGatewayEvent, context: Context, cb: Callback) => {
-  const ebot7Event = JSON.parse(event.body);
-  const {eventName, data} = ebot7Event;
-  const {body: message} = data;
-  const source: Source = data.source;
-
-  if (eventName !== EVENT_MESSAGE_CREATED_NAME) {
-    return cb(null, {statusCode: 404, body: `Invalid event received: ${eventName}`});
-  }
-  if (source === 'visitor') {
-    return cb(null, {statusCode: 404, body: `Invalid source received: ${source}. This endpoint forwards facebook messages only when bot or agent send a message`});
-  }
-
-  // TOOD: send message on facebook
-  
-
-  cb(null, {statusCode: 200});
+interface Response {
+  statusCode: number;
+  headers?: any;
+  body?: string;
 }
+
+export const eventsEndpoint: Handler = async (
+  event: APIGatewayEvent,
+  context: Context
+): Promise<Response> => {
+  const ebot7Event = JSON.parse(event.body);
+  const message = ebot7Event.data.body;
+
+  const invalid = validateEvent(event);
+  if (invalid) return invalid;
+
+  try {
+    const convWrapper = new Ebot7ConvWrapper(environment.appKey);
+    const { botId, convId } = ebot7Event.data;
+    const fbRecipientId = await convWrapper.getConversationFbRecipientId(
+      convId,
+      botId
+    );
+    await sendMessage("TODO-oauth-fb-token", fbRecipientId, message);
+  } catch (e) {
+    return { statusCode: 500, body: e.message };
+  }
+
+  return { statusCode: 200 };
+};
+
+export const redirectEndpoint: Handler = async (
+  event: APIGatewayEvent,
+  context: Context
+): Promise<Response> => {
+  const response = {
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'text/html',
+    },
+    body: 'TODO',
+  };
+  // callback will send HTML back
+  return response
+};
