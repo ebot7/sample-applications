9f34d292ffb0babb98effb5d4925f385747bfe86
Kyrill <kyrill.langhans@e-bot7.com>
2021-06-28 16:45:46 +0200

    [AP-358] Moved installation and ddb process to seperate lambda

---
+++ channel-integration/functions/applicationInstall.ts
@@ -0,0 +1,25 @@
+import { APIGatewayEvent, Context, Handler } from "aws-lambda";
+import { initEnvironment } from "./environment";
+import { putItem } from "./helpers/dynamoClient";
+import { installApplication } from "./helpers/ebot7-application-install";
+
+/**
+ * Installs the application with the specified appKey from environment
+ * to a bot. It expects as body:
+ * { installationAccessToken, botId, pageId, pageAccessToken }
+ * @param event 
+ * @param context 
+ * @returns 
+ */
+export const applicationInstall: Handler = async (
+  event: APIGatewayEvent,
+  context: Context
+) => {
+    const environment = await initEnvironment();
+
+    const {installationAccessToken, botId, pageId, pageAccessToken} = JSON.parse(event.body);
+    const appKey = environment.appKey;
+
+    await putItem(botId, pageId, pageAccessToken);
+    await installApplication(appKey, installationAccessToken);
+};
--- channel-integration/functions/helpers/dynamoClient.ts
+++ channel-integration/functions/helpers/dynamoClient.ts
@@ -1,28 +1,50 @@
 import * as AWS from "aws-sdk";
-import {initEnvironment} from "../environment";
-
+import { initEnvironment } from "../environment";
 
 export const getItem = async (botId: string) => {
-    const environment = await initEnvironment();
+  const environment = await initEnvironment();
 
-    AWS.config.update({...environment.awsConfiguration});
-    const docClient = new AWS.DynamoDB.DocumentClient();
+  AWS.config.update({ ...environment.awsConfiguration });
+  const docClient = new AWS.DynamoDB.DocumentClient();
 
-    return new Promise((resolve, reject) => {
-        const params = {
-            TableName: environment.awsDynamoTable,
-            FilterExpression: "botId = :botId",
-            ExpressionAttributeValues: {
-                ":botId": { S: botId },
-              },
-        };
-    
-        docClient.scan(params, (err, data) => {
-            if (err) console.error('Error on dbScan ', err);
-            else {
-                console.log('dbScan result: ', data);
-                resolve(data.Items[0]);
-            }
-        })
-    })
-}
+  return new Promise((resolve, reject) => {
+    const params = {
+      TableName: environment.awsDynamoTable,
+      FilterExpression: "botId = :botId",
+      ExpressionAttributeValues: {
+        ":botId": { S: botId },
+      },
+    };
+
+    docClient.scan(params, (err, data) => {
+      if (err) console.error("Error on dbScan ", err);
+      else {
+        console.log("dbScan result: ", data);
+        resolve(data.Items[0]);
+      }
+    });
+  });
+};
+
+export const putItem = async (botId: string, pageId: string, pageAccessToken: string) => {
+  const environment = await initEnvironment();
+
+  AWS.config.update({ ...environment.awsConfiguration });
+
+  const docClient = new AWS.DynamoDB.DocumentClient();
+  
+  return new Promise((resolve, reject) => {
+    const params = {
+      TableName: environment.awsDynamoTable,
+      Item: {botId, pageId, pageAccessToken},
+    };
+
+    docClient.put(params, function (err, data) {
+      if (err) {
+        console.log("Error when inserting into DDB", err);
+      } else {
+        console.log("Successfully inserted into DDB", data);
+      }
+    });
+  });
+};
