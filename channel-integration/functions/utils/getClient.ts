3c86a2a8884fc8cdc7d4c4cb66d290ae412c647d
matthew@e-bot7.com <matthew@e-bot7.com>
2021-06-28 08:11:44 +0200

    AP-358 Secrets manager implementation and code cleanup

--- channel-integration/functions/utils/getClient.ts
+++ channel-integration/functions/utils/getClient.ts
@@ -1,22 +1,31 @@
 import { Ebot7Client, Ebot7ConversationClient, Ebot7ExternalConversationClient, Ebot7MessageClient } from '@ebot7/sdk'
+import { SecretsManagerClient, GetSecretValueCommand } from "@aws-sdk/client-secrets-manager";
 /**
  * 
- * @param type The client type that should be returned
  * @returns an e-bot7 API client of the specified type. Authenticated using environment variables specified in serverless.yml.
  */
-export function getClient(type: "convs" | "external-convs" | "messages") {
+export async function getClient() {
+  const { ACCESS_TOKEN: bearerToken } = await getSecretFromSecretsManager(process.env.token)
 	const client = new Ebot7Client({
-		bearerToken: process.env.token,
+		bearerToken,
 		baseURL: process.env.baseUrl,
 	});
-	switch (type) {
-		case "convs":
-			return new Ebot7ConversationClient(client)
-		case "external-convs":
-			return new Ebot7ExternalConversationClient(client)
-		case "messages":
-			return new Ebot7MessageClient(client)
-		default:
-			throw new Error("unknown client type")
+	return {
+		convs: new Ebot7ConversationClient(client),
+		externalConvs: new Ebot7ExternalConversationClient(client),
+		messages: new Ebot7MessageClient(client),
 	}
+}
+
+// This function retrieves a secret from AWS Secrets Manager using an env variable as a key.
+// This is not part of the e-bot7 Application Platform, it is just a basic implementation of a secure mechanism for storing keys.
+async function getSecretFromSecretsManager(secretId) {
+  const sm = new SecretsManagerClient({
+    region: 'eu-central-1'
+  })
+  const command = new GetSecretValueCommand({
+    SecretId: secretId
+  })
+  const response = await sm.send(command)
+  return JSON.parse(response.SecretString)
 }
