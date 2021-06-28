df295de08f0b86d16ce23ac9e46aac4c8610ba1e
matthew@e-bot7.com <matthew@e-bot7.com>
2021-06-28 15:32:33 +0200

    AP-358 Move inbound flow to same stack

--- channel-integration/functions/outbound.ts
+++ channel-integration/functions/outbound.ts
@@ -1,3 +1,47 @@
-export function handler(event) {
-	console.log('should sound outbound message', event.body)
+import { APIGatewayEvent, Context, Handler } from "aws-lambda";
+import { Ebot7ConvWrapper } from "./helpers/ebot7-conv-wrapper";
+import { sendMessage } from "./helpers/facebook-send";
+import {initEnvironment} from "./environment";
+import { validateEvent } from "./helpers/validate-event";
+import { getItem } from "./helpers/dynamoClient";
+
+interface Response {
+  statusCode: number;
+  headers?: any;
+  body?: string;
 }
+
+export const eventsEndpoint: Handler = async (
+  event: APIGatewayEvent,
+  context: Context
+): Promise<Response> => {
+  const environment = await initEnvironment();
+  
+  const ebot7Event = JSON.parse(event.body);
+  console.log(ebot7Event);
+  
+  const message = ebot7Event.data.body;
+
+  const invalid = validateEvent(event);
+  if (invalid) return invalid;
+
+  try {
+    const convWrapper = new Ebot7ConvWrapper(environment.appKey);
+    const { botId, convId } = ebot7Event.data;
+    const fbRecipientId = await convWrapper.getConversationFbRecipientId(
+      convId,
+      botId
+    );
+    const cleanMessage = message.replace(/<\/?[^>]+(>|$)/g, ""); // remove html tags
+    const dynamoItem = await getItem(botId);
+    console.log('Dynamo Item: ', dynamoItem);
+    
+    await sendMessage((dynamoItem as any).pageAccessToken, fbRecipientId, cleanMessage);
+    console.log('Sent message');
+  } catch (e) {
+    console.error(e);
+    return { statusCode: 500, body: e.message };
+  }
+
+  return { statusCode: 200 };
+};
