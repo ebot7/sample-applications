cc75e7cca884b925642783fecf163d69c85a1407
matthew@e-bot7.com <matthew@e-bot7.com>
2021-06-29 22:26:50 +0200

    AP-358 Remove duplicate code

--- channel-integration/functions/helpers/getClient.ts
+++ channel-integration/functions/helpers/getClient.ts
@@ -1,5 +1,6 @@
 import { Ebot7Client, Ebot7ConversationClient, Ebot7ExternalConversationClient, Ebot7MessageClient } from '@ebot7/sdk'
 import { SecretsManagerClient, GetSecretValueCommand } from "@aws-sdk/client-secrets-manager";
+import { getSecretFromSecretsManager } from './get-secret-from-secrets-manager';
 /**
  * 
  * @returns an e-bot7 API client of the specified type. Authenticated using environment variables specified in serverless.yml.
@@ -15,17 +16,4 @@ export async function getClient() {
 		externalConvs: new Ebot7ExternalConversationClient(client),
 		messages: new Ebot7MessageClient(client),
 	}
-}
-
-// This function retrieves a secret from AWS Secrets Manager using an env variable as a key.
-// This is not part of the e-bot7 Application Platform, it is just a basic implementation of a secure mechanism for storing keys.
-async function getSecretFromSecretsManager(secretId) {
-  const sm = new SecretsManagerClient({
-    region: 'eu-central-1'
-  })
-  const command = new GetSecretValueCommand({
-    SecretId: secretId
-  })
-  const response = await sm.send(command)
-  return JSON.parse(response.SecretString)
 }
