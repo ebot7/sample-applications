2239788a78aa957c8fa6b5c4200ff634739370e3
Kyrill <kyrill.langhans@e-bot7.com>
2021-06-29 09:21:40 +0200

    [AP-358] Replaced hardcoded sender id

--- channel-integration/functions/helpers/ebot7-conv-wrapper.ts
+++ channel-integration/functions/helpers/ebot7-conv-wrapper.ts
@@ -1,13 +1,16 @@
-import {Ebot7Client} from '@ebot7/sdk';
+import {Ebot7Client, Ebot7ConversationClient} from '@ebot7/sdk';
 
 export class Ebot7ConvWrapper {
     private static BASE_URL = `https://api.staging.e-bot7.de`;
+    client: Ebot7ConversationClient;
 
     constructor(appToken: string) {
-        const client = new Ebot7Client({bearerToken: appToken, baseURL: Ebot7ConvWrapper.BASE_URL});
+        const generalClient = new Ebot7Client({bearerToken: appToken, baseURL: Ebot7ConvWrapper.BASE_URL});
+        this.client = new Ebot7ConversationClient(generalClient);
     }
 
     async getConversationFbRecipientId(convId: string, botId: string) {
-        return '3994360700681664'; // TODO: As soon as inbound flow done, remove hardcoded value
+        const conv = await this.client.findOne({botId, convId});
+        return conv.item.externalData.find(x => x.name === 'facebook').id.split('-')[0]; // should be <SENDER-ID>-<PAGE-ID>
     }
 }
